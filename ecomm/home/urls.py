4d6c85106e78be5d5ec87571e811caf3b6f331e2
Lalit Joshi <lalit2055joshi@gmail.com>
2022-09-22 18:42:39 +0545

    login signin

--- ecomm/home/urls.py
+++ ecomm/home/urls.py
@@ -1,10 +1,15 @@
 from operator import imod
+from re import search
 from django.urls import path
 from .views import *
 
 urlpatterns = [
     path('',HomeView.as_view(),name='home'),
     path('details/<slug>',ProductDetailView.as_view(),name='details'),
-    path('add_review',review,name='add_review')
+    path('add_review',review,name='add_review'),
+    path('category/<slug>',CategoryView.as_view(),name='category'),
+    path('signup',signup,name='signup'),
+    path('login',login,name='login'),
+
     
 ]
--- ecomm/home/views.py
+++ ecomm/home/views.py
@@ -1,8 +1,12 @@
 from itertools import product
-from django.shortcuts import render,redirect
+from pickle import NONE
+from pyexpat.errors import messages
+from django.shortcuts import render , redirect
 from .models import *
 from django.views.generic import View
 import datetime
+from django.contrib.auth.models import User
+from django.contrib import messages,auth
 # Create your views here.
 
 class Base(View):
@@ -31,7 +35,7 @@ class ProductDetailView(Base):
         self.views['details'] = Product.objects.filter(slug = slug )
         self.views['reviews'] = Review.objects.filter(slug = slug )
         subcat = Product.objects.get(slug = slug).subcategory
-        
+
         self.views['subcat_product']=Product.objects.filter(subcategory = subcat)
         
         return render(request,'product-detail.html',self.views)
@@ -56,5 +60,64 @@ def review(request):
 
         return redirect(f'/details/{slug}')
 
+class CategoryView(Base):
+    def get(self,request,slug):
+        self.views
+        cat_id = Categories.objects.get(slug = slug).id
+        self.views['cat_product'] = Product.objects.filter(category_id = cat_id)
+        return render(request,'product-list.html',self.views)        
 
+class SearchView(Base):
+    def get(self,request):
+        self.views
+        if request.method =="GET":
+            query=request.GET['query']
+            self.views['search_product']= Product.objects.filter(name__icontains = query)
+            self.views['search_for']=query
+        return render(request,'search.html',self.views)        
 
+def signup(request):
+    if request.method == 'POST':
+        f_name = request.POST['first_name']
+        l_name = request.POST['last_name']
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        cpassword = request.POST['cpassword']
+
+        if password == cpassword:
+            if User.objects.filter(username=username).exists():
+                messages.error(request,'the username is already taken')
+                return redirect('/signup')
+            elif User.objects.filter(email=email).exists():
+                 messages.error(request,'the username is already taken')
+                 return redirect('/signup')
+            else:
+                data = User.objects.create_user(
+                    username=username,
+                    email=email,
+                    password=password,
+                    first_name =f_name,
+                    last_name = l_name
+                )
+                data.save()
+                return redirect('/')
+
+        else:
+            messages.error(request,'password does not match')    
+    return render(request,'signup.html')
+
+from django.contrib.auth import login , logout
+
+def login(request):
+    if request.method == 'POST':
+        username=request.POST["username"]
+        password = request.POST["password"]
+        user = auth.autenticate(username = username , password = password )
+        if user is not NONE:
+            auth.login(request,user)
+            return redirect('/')
+        else:
+            messages.error(request,'The User Name Or Password does not match')
+            return redirect('/login')    
+    return render(request,'login.html')        
